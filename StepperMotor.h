9687fb5ec6c589b003a1cdc41efce659dd26b972
Shmuel Meymann <shmuel@nowforce.com>
2015-09-12 21:59:33 +0300

    Added the basic include files

--- StepperMotor.h
+++ StepperMotor.h
@@ -1,6 +1,12 @@
 #ifndef __STEPPER_MOTOR_H__
 #define __STEPPER_MOTOR_H__
 
+#if defined(ARDUINO) && ARDUINO >= 100
+#include "Arduino.h"
+#else
+#include "WProgram.h"
+#endif
+
 class StepperMotor {
 public:
   StepperMotor(float stride, int c1, int c2, int c3, int c4);
